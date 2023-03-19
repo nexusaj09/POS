67924d8b7d72f8e604d78d7d031886ba96f2fb4f
Rehmard Butad <rbutad@outlook.com>
2023-03-19 12:56:01 +0800

    GCashTransactions table migration

---
+++ POS.Database.Migrator/Migrations/2023/03/Migration202303191250_RenameTypeColumnInGCashTransactionsTable.cs
@@ -0,0 +1,18 @@
+﻿using POS.Database.Migrator.Attributes;
+
+namespace POS.Database.Migrator.Migrations._2023._03;
+
+[DbMigration(1, 2023, 03, 19, 12, 50, "rbutad")]
+public class Migration202303191250_RenameTypeColumnInGCashTransactionsTable : BaseDbMigration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"
+ALTER TABLE [dbo].[GCashTransactions]
+DROP COLUMN [Type];
+
+ALTER TABLE [dbo].[GCashTransactions]
+ADD TransactionType INT NOT NULL;
+");
+    }
+}
