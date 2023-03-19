cc04aab0c06bf95ee163b8aa20d0d75fb64a7c20
Rehmard Butad <rbutad@outlook.com>
2023-03-19 12:48:56 +0800

    Create extension for IDataReader

---
+++ POS/Extensions/DataReaderExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace POS.Extensions
+{
+    public static class DataReaderExtension
+    {
+        public static List<T> ConvertToList<T>(this IDataReader reader) where T : class
+        {
+            var list = new List<T>();
+            T obj = default(T);
+
+            while (reader.Read())
+            {
+                obj = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                {
+                    if (!Equals(reader[prop.Name], DBNull.Value))
+                    {
+                        prop.SetValue(obj, reader[prop.Name], null);
+                    }
+                }
+
+                list.Add(obj);
+            }
+
+            return list;
+        }
+
+        public static T ConvertToObject<T>(this IDataReader reader) where T : class, new()
+        {
+            T obj = new T();
+
+            while (reader.Read())
+            {
+                obj = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                {
+                    if (!Equals(reader[prop.Name], DBNull.Value))
+                    {
+                        prop.SetValue(obj, reader[prop.Name], null);
+                    }
+                }
+            }
+
+            return obj;
+        }
+    }
+}
