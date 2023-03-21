8b0bec87938b83b329cd8599e4a0da258de84eb1
Rehmard Butad <rbutad@outlook.com>
2023-03-21 19:28:17 +0800

    Alter modified columns to be nullable

---
+++ POS.Database.Migrator/Migrations/2023/03/Migration202303211925_GCashTransactionsTable_AlterModifiedColumnsToBeNullable.cs
@@ -0,0 +1,18 @@
+﻿using POS.Database.Migrator.Attributes;
+
+namespace POS.Database.Migrator.Migrations._2023._03;
+
+[DbMigration(1, 2023, 03, 21, 19, 25, "rbutad")]
+public class Migration202303211925_GCashTransactionsTable_AlterModifiedColumnsToBeNullable : BaseDbMigration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"
+ALTER TABLE [GCashTransactions]
+ALTER COLUMN LastModifiedByID INT NULL
+
+ALTER TABLE [GCashTransactions]
+ALTER COLUMN LastModifiedDateTime DATETIME NULL
+");
+    }
+}
