0c8eae26fd774df23f9504404e54eab52e2f5d4e
Rehmard Butad <rbutad@outlook.com>
2023-03-26 14:04:03 +0800

    Load discount list

--- POS/Panels/PanelDiscounts.cs
+++ POS/Panels/PanelDiscounts.cs
@@ -1,18 +1,39 @@
 ﻿using MetroFramework.Forms;
 using POS.Forms;
+using POS.Helpers;
 
 namespace POS.Panels
 {
     public partial class PanelDiscounts : MetroForm
     {
+        private readonly DiscountHelper _discountHelper;
+
         public PanelDiscounts(FormTransaction fromTran)
         {
             InitializeComponent();
+
+            _discountHelper = new DiscountHelper();
         }
 
         private void PanelDiscounts_Load(object sender, System.EventArgs e)
         {
+            _discountHelper.LoadDiscount(gridDiscount, txtSearch.Text);
 
+            Init();
+        }
+
+        private protected void Init()
+        {
+            FocusSearch();
+
+            gridDiscount.ClearSelection();
+            gridDiscount.CurrentCell = null;
+        }
+
+        private protected void FocusSearch()
+        {
+            txtSearch.Select();
+            txtSearch.Focus();
         }
     }
 }
