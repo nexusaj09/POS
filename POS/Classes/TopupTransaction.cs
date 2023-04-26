b1f0f47db554b87380975306ac92e3269f2027a7
Rehmard Butad <rbutad@outlook.com>
2023-04-26 17:47:42 +0800

    Model for TopupTransaction

---
+++ POS/Classes/TopupTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Classes
+{
+    public class TopupTransaction : Audit
+    {
+        public int ID { get; set; }
+        public string ReferenceNumber { get; set; }
+        public decimal Amount { get; set; }
+        public int TopupType { get; set; }
+        public int ShiftID { get; set; }
+    }
+}
