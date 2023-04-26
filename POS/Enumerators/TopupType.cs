a962cc4fdbc09a5badf910dec4292ceef171ec56
Rehmard Butad <rbutad@outlook.com>
2023-04-26 17:47:32 +0800

    Enum for TopupType

---
+++ POS/Enumerators/TopupType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace POS.Enumerators
+{
+    public enum TopupType
+    {
+        [Description("Top-up GCash")]
+        TopupGCash = 3,
+        [Description("Top-up Drawer")]
+        TopupDrawer = 4
+    }
+}
