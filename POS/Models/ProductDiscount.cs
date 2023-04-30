386d97428d77011923a1bbdc5488251c06dd987c
Rehmard Butad <rbutad@outlook.com>
2023-04-30 19:48:14 +0800

    Model for ProductDiscount

---
+++ POS/Models/ProductDiscount.cs
@@ -0,0 +1,13 @@
+﻿using POS.Classes;
+
+namespace POS.Models
+{
+    public class ProductDiscount : Audit
+    {
+        public int Id { get; set; }
+        public string ProductCode { get; set; }
+        public int DiscountID { get; set; }
+        public string DiscountDescription { get; set; }
+        public decimal DiscountPercentage { get; set; }
+    }
+}
