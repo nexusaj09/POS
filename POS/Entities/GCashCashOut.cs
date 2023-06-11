b828bf6ea9dde3a00e79caf01fab72e2e8660578
Rehmard Butad <rbutad@outlook.com>
2023-06-11 17:41:40 +0800

    Entity for GCashCashOuts

---
+++ POS/Entities/GCashCashOut.cs
@@ -0,0 +1,17 @@
+﻿using POS.Classes;
+using System;
+
+namespace POS.Entities
+{
+    public class GCashCashOut : Audit
+    {
+        public Guid Uid { get; set; }
+        public string ReferenceNumber { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Fee { get; set; }
+        public bool IsFeePaySeparately { get; set; }
+        public bool IsAmountIncludesFee { get; set; }
+        public bool IsFeeDeductedOnCashOutAmount { get; set; }
+        public int ShiftID { get; set; }
+    }
+}
